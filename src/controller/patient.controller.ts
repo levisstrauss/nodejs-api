87803eddf3f4eb13746a822d8e0a6707f3732095
levisstrauss <levisstrauss11@yahoo.com>
2024-07-21 20:44:15 -0400

    Code the controller part 2

--- src/controller/patient.controller.ts
+++ src/controller/patient.controller.ts
@@ -5,15 +5,31 @@ import { QUERY } from "../query/patient.query";
 import { Code } from "../enum/code.enum";
 import { HttpResponse } from "../domain/response";
 import { Status } from "../enum/status.enum";
-import { OkPacketParams, ResultSetHeader, RowDataPacket } from "mysql2";
+import { FieldPacket, OkPacket, OkPacketParams, ResultSetHeader, RowDataPacket } from "mysql2";
 
+type resultSet = [RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader, FieldPacket[]];
 
-// Where We gonna have all the logic 
+// Get all the patients
 export const getPatients = async (req: Request, rest: Response): Promise<Response<Patient[]>> => {
     console.info(`[${new Date().toLocaleString}] Incomming ${req.method} ${req.originalUrl} Request from ${req.rawHeaders[0]} ${req.rawHeaders[1]}`);
     try{
         const pool = await connection(); 
-        const result: any = await pool.query(QUERY.SELECT_PATIENTS);
+        const result: resultSet = await pool.query(QUERY.SELECT_PATIENTS);
+        return rest.status(Code.OK)
+            .send(new HttpResponse(Code.OK, Status.OK, 'Patients Retrieved', result[0]));
+    }catch(error: unknown){
+        console.error(error);
+        return rest.status(Code.INTERNAL_SERVER_ERROR)
+            .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
+    }
+}
+
+// Get one Patient
+export const getPatient = async (req: Request, rest: Response): Promise<Response<Patient[]>> => {
+    console.info(`[${new Date().toLocaleString}] Incomming ${req.method} ${req.originalUrl} Request from ${req.rawHeaders[0]} ${req.rawHeaders[1]}`);
+    try{
+        const pool = await connection(); 
+        const result: resultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
         return rest.status(Code.OK)
             .send(new HttpResponse(Code.OK, Status.OK, 'Patients Retrieved', result[0]));
     }catch(error: unknown){
