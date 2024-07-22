564110ba819e06b0cc09c937f382268cde78724d
levisstrauss <levisstrauss11@yahoo.com>
2024-07-21 20:57:24 -0400

    Create a patient

--- src/controller/patient.controller.ts
+++ src/controller/patient.controller.ts
@@ -25,13 +25,38 @@ export const getPatients = async (req: Request, rest: Response): Promise<Respons
 }
 
 // Get one Patient
-export const getPatient = async (req: Request, rest: Response): Promise<Response<Patient[]>> => {
+export const getPatient = async (req: Request, rest: Response): Promise<Response<Patient>> => {
     console.info(`[${new Date().toLocaleString}] Incomming ${req.method} ${req.originalUrl} Request from ${req.rawHeaders[0]} ${req.rawHeaders[1]}`);
     try{
         const pool = await connection(); 
         const result: resultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
-        return rest.status(Code.OK)
-            .send(new HttpResponse(Code.OK, Status.OK, 'Patients Retrieved', result[0]));
+        if((result[0] as Array<resultSet>).length > 0){
+            return rest.status(Code.OK)
+            .send(new HttpResponse(Code.OK, Status.OK, 'Patient Retrieved', result[0]));
+        }else{
+            return rest.status(Code.NOT_FOUND)
+            .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
+        }
+    }catch(error: unknown){
+        console.error(error);
+        return rest.status(Code.INTERNAL_SERVER_ERROR)
+            .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
+    }
+}
+
+// Createa patient
+export const createPatient = async (req: Request, rest: Response): Promise<Response<Patient[]>> => {
+    console.info(`[${new Date().toLocaleString}] Incomming ${req.method} ${req.originalUrl} Request from ${req.rawHeaders[0]} ${req.rawHeaders[1]}`);
+    try{
+        const pool = await connection(); 
+        const result: resultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
+        if((result[0] as Array<resultSet>).length > 0){
+            return rest.status(Code.OK)
+            .send(new HttpResponse(Code.OK, Status.OK, 'Patient Retrieved', result[0]));
+        }else{
+            return rest.status(Code.NOT_FOUND)
+            .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
+        }
     }catch(error: unknown){
         console.error(error);
         return rest.status(Code.INTERNAL_SERVER_ERROR)
